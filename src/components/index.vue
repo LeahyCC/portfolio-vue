d3a2983955c0f8ee7922e5ab8821195c5bfd15ef
Colin Leahy <leahycc@pm.me>
2018-04-05 12:54:55 -0500

    updated content with more details

--- src/components/index.vue
+++ src/components/index.vue
@@ -24,7 +24,6 @@
             <li>Local Storage</li>
             <li>data bindings</li>
             <li>svg</li>
-            <li>canvas</li>
           </ul>
         </li>
         <li>CSS <span>5 years</span>
@@ -34,6 +33,12 @@
             <li>Flex Box</li>
             <li>animation</li>
             <li>@keyframe</li>
+          </ul>
+        </li>
+        <li>Adobe <span>5 years</span>
+          <ul>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
           </ul>
         </li>
         <li>Javascript <span>3 years</span>
@@ -51,6 +56,12 @@
             <li>cmd - Windows / iOS</li>
           </ul>
         </li>
+        <li>Frameworks <span>3 years</span>
+          <ul>
+            <li>Bootstrap</li>
+            <li><a href="https://www.material-ui.com/" target="_blank">Material-UI</a></li>
+          </ul>
+        </li>
         <li>Webpack, GulpJS, GruntJS <span>2 years</span></li>
         <li>AngualrJS <span>1 year</span></li>
         <li>PHP, Ruby on Rails w/ Homebrew <span>1 year</span></li>
@@ -60,25 +71,77 @@
 
     <h3>Work Experience</h3>
     <div class="work">
+      <h4>Web Developer</h4>
+      <h5>EMP Marketing <span>January '18 - Current</span></h5>
+      <p>
+        Contracted by EMP Marketing to develop a Website for loan lead generation. Using vue-cli and Google Firebase, I created SPA that submits to a real-time database and is accessible only through authentication login-page. There you can retreive all submitted information, edit and delete submissions. The forms are dynamic based off user selection. Site is currently under quality control and will be going live within the next couple weeks.
+      </p>
+      <p>
+        Along with the SPA forms, I built a blog using Wix site generator to serve as a SEO landing-page. I used Wix for a user freindly platform for non-tech people to have acecess to adding, and modifying the blog entries.
+      </p>
+      <h6>Technology Used - High Level</h6>
+      <ul>
+        <li><a href="https://webpack.js.org/" target="_blank">Webpack</a></li>
+        <li><a href="https://github.com/vuejs/vue-cli" target="_blank">Vue-cli</a></li>
+        <li><a href="https://firebase.google.com/products/realtime-database/" target="_blank">Google Firebase ( realtime-database )</a></li>
+        <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex ( state management )</a></li>
+        <li><a href="https://router.vuejs.org/en/" target="_blank">vue-router</a></li>
+        <li><a href="https://github.com/MatteoGabriele/vue-analytics" target="_blank">vue-analytics</a></li>
+        <li><a href="https://yarnpkg.com/en/" target="_blank">Yarn</a></li>
+      </ul>
+      <h6>Link</h6>
+      <ul>
+        <li><a href="https://www.loanllama.com" target="_blank">loanllama.com</a></li>
+        <li><a href="https://www.blog.loanllama.com" target="_blank">blog.loanllama.com</a></li>
+      </ul>
+    </div>
+    <div class="work">
+      <hr>
       <h4>Front-end Developer</h4>
       <h5>Insubuy Insurance Broker <span>January '17 - January '18</span></h5>
       <p>
-        A fast past and agile environment. Insubuy is a leading insurance broker offering a wide range of insurance policies. The site serves as a quote generator to provide a narrowed down list of insurance plans through our backend Java API. Using VueJS, jQuery, and SASS were our technology stack while working closely with Java developers. Along with many other responsibilities I handled the production deployments and the local development environment through GulpJS build process. We used BEM naming convention for our SCSS for a more maintainable and reusable sass architecture. I was also project lead for our Google tag manager for Google dynamic ads and Facebook dynamic ads. Serving targeted advertising to a unique and specific audience. Also worked with 3rd party companies for analytics and site search features.
+        A fast past and agile environment. Insubuy is a leading insurance broker offering a wide range of insurance policies. The site serves mainly as a quote generator to provide a narrowed down list of insurance plans through our backend Java API. Along with many other responsibilities, I handled the production deployments and the local development environment through Gulp JS build process. We used BEM naming convention for our SCSS for a more maintainable and reusable sass architecture. I was project lead for our Google tag manager for Google dynamic ads and Facebook dynamic ads. Serving targeted advertising to a unique and specific audience. Also worked with 3rd party companies for analytics and site search features.
       </p>
+      <p>
+        Insubuy also provides a unique comprehensive, white-labeled web site for affiliated partners. No other wholesaler in the market provides such services to their partners.
+      </p>
+      <h6>Technology Used - High Level</h6>
+      <ul>
+        <li><a href="https://vuejs.org/" target="_blank">Vue JS</a></li>
+        <li><a href="https://webpack.js.org/" target="_blank">Webpack</a></li>
+        <li><a href="https://gulpjs.com/" target="_blank">Gulp JS</a></li>
+        <li><a href="https://github.com/jkphl/gulp-svg-sprite" target="_blank">Gulp SVG Sprite</a></li>
+        <li><a href="https://browsersync.io/" target="_blank">Browsersync</a></li>
+        <li><a href="https://yarnpkg.com/en/" target="_blank">Yarn</a></li>
+      </ul>
+      <h6>Link</h6>
+      <ul>
+        <li><a href="https://www.insubuy.com/" target="_blank">insubuy.com</a></li>
+      </ul>
     </div>
     <div class="work">
+      <hr>
       <h4>Web Developer</h4>
       <h5>TNT Dental Web marketing <span>November '15 - December '16</span></h5>
       <p>
         Develop websites from pre-production design stage through post QA edits. With over 100 full websites completed, utilizing HTML5, SASS, and Javascript / jQuery. Built websites according to best practices at a high company standard of quality assurance for all major device resolutions. Ensured all websites are fully responsive and cross-browser compatible without the use of template frameworks. Frequently chosen to train junior developers and support team. Also started training in Ruby on Rails for our backend CMS working along side the companies lead developer to handle client site deployments and their CMS.
       </p>
+      <h6>Link</h6>
+      <ul>
+        <li><a href="https://www.tntdental.com/" target="_blank">tntdental.com</a></li>
+      </ul>
     </div>
     <div class="work">
+      <hr>
       <h4>Web Content Specialist</h4>
       <h5>Mauri Pro Sailing eCommerce <span>March '15 - October '15</span></h5>
       <p>
         Launched several new product categories containing thousands of new product pages per category through Dreamweaver and uploaded them to the CMS. Created higher standard of HTML templates for company-wide implementation which allowed for flexible product description layouts and ease of maintenance.
       </p>
+      <h6>Link</h6>
+      <ul>
+        <li><a href="https://www.mauriprosailing.com/" target="_blank">mauriprosailing.com</a></li>
+      </ul>
     </div>
 
     <h3>Education</h3>
